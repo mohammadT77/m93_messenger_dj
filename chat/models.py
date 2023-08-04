06630bb05ae96c455b7b5cf69e94fff003321118
matehrani <t.mohammadamin@gmail.com>
2023-08-04 12:58:55 +0200

    Create Message model

--- chat/models.py
+++ chat/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-from core.models import BaseModel
+from core.models import BaseModel, User
 
 # Create your models here.
-class Message(BaseModel):
-    pass  # TODO
 
-class Chat(BaseModel):
-    pass  # TODO
+class Message(BaseModel):
+    subject = models.CharField(max_length=40)
+    sender = models.ForeignKey(User, models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, models.CASCADE, related_name='received_messages')
+    content = models.CharField(max_length=400)
+    replay_ref = models.ForeignKey("Message", models.SET_NULL, null=True, default=None, blank=True)
+    seen = models.BooleanField(default=False, null=False, blank=False)
+    seen_timestamp = models.DateTimeField(default=None, null=True, blank=True)
