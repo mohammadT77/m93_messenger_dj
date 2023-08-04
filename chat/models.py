39050398a137686a808553c3717bddf8bb0393ec
matehrani <t.mohammadamin@gmail.com>
2023-08-04 10:29:21 +0200

    Init app chat

---
+++ chat/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from core.models import BaseModel
+
+# Create your models here.
+class Message(BaseModel):
+    pass  # TODO
+
+class Chat(BaseModel):
+    pass  # TODO
