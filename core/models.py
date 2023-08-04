0a5bac9586543a8baf904c82dc9d9822b7b0ec53
matehrani <t.mohammadamin@gmail.com>
2023-08-04 10:27:44 +0200

    Implement BaseManager

--- core/models.py
+++ core/models.py
@@ -1,21 +1,45 @@
+from typing import Any, Dict, Tuple
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
+from django.db.models.query import QuerySet
 
 # Create your models here.
+class BaseManager(models.Manager):
+
+    # TODO: implement Logical Delete here!
+    
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(is_deleted=False)
+
+    def archive(self):
+        return super().get_queryset()
+
+
 class BaseModel(models.Model):
     class Meta:
         abstract = True
+
+    objects = BaseManager()
     
+    is_deleted = models.BooleanField(default=False, db_index=True, null=False, blank=False)
     create_timestamp = models.DateTimeField(auto_now_add=True)
+    delete_timestamp = models.DateTimeField(default=None, null=True, blank=True)
+    modify_timestamp = models.DateTimeField(auto_now=True)
+
+    def delete(self, **kwargs) -> Tuple[int, Dict[str, int]]:
+        self.is_deleted = True
+        self.delete_timestamp = datetime.now()
+        self.save(using=kwargs.get("using"))
 
 
 class User(AbstractUser):
     pass
 
-
 # Abstract inheritance example
 class Product(BaseModel):
+    name = models.CharField(max_length=20)
     pass
 
 
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import User, Product
 
 # Create your tests here.
+class MyTest(TestCase):
+
+    def test1(self):
+        p1 = Product.objects.create(name="p1")
+        p2 = Product.objects.create(name="p2")
+
+        print("all before:", Product.objects.all())
+        Product.objects.all().delete()
+        print("p2.is_deleted:", p2.is_deleted)
+        print("Archive:", Product.objects.archive())
+        print("All:", Product.objects.all())
