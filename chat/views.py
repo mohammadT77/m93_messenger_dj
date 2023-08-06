51c94a2d3b756d6232bfeec9c03144712a5ca899
Abbas moradi <en.moradi.66@gmail.com>
2023-08-06 18:21:41 +0330

    add a show msg all/by id & create msg

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,10 @@
 from django.http.response import JsonResponse
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ChatSerializers
+from .models import Message
+from django.db.models import Q
+
 
 def index_view(req):
     data = {
@@ -7,6 +12,19 @@ def index_view(req):
     }
     return JsonResponse(data)
 
-# @csrf_exempt
-# def index_post(req):
-#     pass
+class ChatView(APIView):
+    def get(self, request, user_id=None):
+        query_set = Message.objects.all()
+        serializers_data =  ChatSerializers(query_set, many=True)
+        if user_id:
+            query_set = Message.objects.filter(Q(sender=user_id) | Q(receiver=user_id))
+            serializers_data =  ChatSerializers(query_set, many=True)
+        return Response(serializers_data.data)
+        
+
+    def post(self, request):
+        serializers_data = ChatSerializers(data=request.POST)
+        if serializers_data.is_valid():
+            serializers_data.create(serializers_data.validated_data)
+            return Response(serializers_data.data)
+        return Response(serializers_data.errors)
